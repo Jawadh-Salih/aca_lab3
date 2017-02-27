a11903dd42820a68c8ee7ae66e16324e9e1dadf4
KRVPerera <rukshan.viduranga@gmail.com>
2017-02-27 13:28:46 +0530

    Added necessary changes to measure time, required sample sizes

--- util.c
+++ util.c
@@ -4,6 +4,76 @@
 
 #include "util.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+// N = (100zs/rx)^2, r - accuracy (5%), s - standard deviation, x - mean, z = 1.960 (95%) - TAKE THE CEIL
+/// This function gives the required number of samples to take to achieve
+/// accuracy of 5% and 95% confidence
+/// \param sd standard deviation
+/// \param mean average
+/// \return
+
+long requiredSampleSize(float sd, float mean) {
+    long N = (long) ceil(((float) 100 * 1.960 * sd) / (5 * mean));
+    return N;
+}
+
+double mean(double times[], int numSamples) {
+    double sum = 0;
+    for (int i = 0; i < numSamples; ++i) {
+        sum += times[i];
+    }
+    return (double) sum / numSamples;
+}
+
+double standardDeviation(float times[], int numSamples) {
+    double u = mean(times,numSamples);
+    double variance = 0;
+    for (int i = 0; i < numSamples; ++i) {
+        variance += pow(times[i] - u, 2);
+    }
+    variance = variance / numSamples;
+    return sqrt(variance);
+}
+
+float elapsed_time_nsec(struct timespec *begin, struct timespec *end,
+                              unsigned long *sec, unsigned long *nsec) {
+    if (end->tv_nsec < begin->tv_nsec) {
+        *nsec = 1000000000 - (begin->tv_nsec - end->tv_nsec);
+        *sec = end->tv_sec - begin->tv_sec - 1;
+    } else {
+        *nsec = end->tv_nsec - begin->tv_nsec;
+        *sec = end->tv_sec - begin->tv_sec;
+    }
+    return (float) (*sec) * 1000000 + ((float) (*nsec));
+}
+
+
+float elapsed_time_microsec(struct timespec *begin, struct timespec *end,
+                                  unsigned long *sec, unsigned long *nsec) {
+    if (end->tv_nsec < begin->tv_nsec) {
+        *nsec = 1000000000 - (begin->tv_nsec - end->tv_nsec);
+        *sec = end->tv_sec - begin->tv_sec - 1;
+    } else {
+        *nsec = end->tv_nsec - begin->tv_nsec;
+        *sec = end->tv_sec - begin->tv_sec;
+    }
+    return (float) (*sec) * 1000000 + ((float) (*nsec)) / 1000.0;
+}
+
+float elapsed_time_msec(struct timespec *begin, struct timespec *end,
+                              unsigned long *sec, unsigned long *nsec) {
+    if (end->tv_nsec < begin->tv_nsec) {
+        *nsec = 1000000000 - (begin->tv_nsec - end->tv_nsec);
+        *sec = end->tv_sec - begin->tv_sec - 1;
+    } else {
+        *nsec = end->tv_nsec - begin->tv_nsec;
+        *sec = end->tv_sec - begin->tv_sec;
+    }
+    return (float) (*sec) * 1000 + ((float) (*nsec)) / 1000000.0;
+}
 
 int main(){
 
--- util.h
+++ util.h
@@ -5,8 +5,8 @@
 #ifndef ACA_LAB3_UTIL_H
 #define ACA_LAB3_UTIL_H
 
-#endif //ACA_LAB3_UTIL_H
-
 void example(void);
 static void matvec_simple(int n, float vec_c[n],const float mat_a[n][n] , const float vec_b[n]);
 static void matvec_unrolled(int n, float vec_c[n],const float mat_a[n][n] , const float vec_b[n]);
+
+#endif //ACA_LAB3_UTIL_H
