48752b571f213b0f4762fa6dd85dcd9300fb5ba8
KRVPerera <rukshan.viduranga@gmail.com>
2017-02-28 14:40:53 +0530

    Main changed to gather information and run with 95% confidence interval for average value

--- main.c
+++ main.c
@@ -3,8 +3,13 @@
  */
 
 #include <stdio.h>
-//#include <stdlib.h>
+#include <stdlib.h>
 #include<pmmintrin.h>
+#include <getopt.h>
+#include <time.h>
+#include "util.h"
+#include "matvec_drivers.h"
+
 //#include <assert.h>
 //#include<smmintrin.h>
 //#include<tmmintrin.h>
@@ -84,23 +89,86 @@ int main(int argc, char *argv[]) {
     float vec_seq[8]={0};
     float vec_sse[8]={0};
 
-    printf("Loop unrolled Answer\n");
-    matvec_unrolled(n, vec_seq, mat, vec);
-    for (int i = 0; i < n; ++i) {
-        printf("%f ", vec_seq[i]);
+
+    short mat_vec_ver = 0, mat_mat_ver = 0, c_ver = 0, sse_ver = 0, a_vec_ver = 0;
+
+    while ((c = getopt(argc, argv, "n:hvmcsa")) != -1) {
+        switch (c) {
+            case 'n':
+                n = atoi(optarg);
+                break;
+            case 'v':
+                mat_vec_ver = 1;
+                break;
+            case 'c':
+                c_ver = 1;
+                break;
+            case 's':
+                sse_ver = 1;
+                break;
+            case 'a':
+                a_vec_ver = 1;
+                break;
+            case 'm':
+                mat_mat_ver = 1;
+                break;
+            case 'h':
+                printf("-m\t\t\t-\trun the matrix X matrix version.\n");
+                printf("-v\t\t\t-\trun the matrix X vector version.\n");
+                printf("-c\t\t\t-\trun the cpu version.\n");
+                printf("-s\t\t\t-\trun the sse version.\n");
+                printf("-a\t\t\t-\trun the auto vectorized version.\n");
+                printf("-h\t\t\t-\tShow this menu\n");
+                printf("example for the assignment.\n");
+                printf("eg : ./aca_lab3 -n 100 -v\n");
+                printf("eg : ./aca_lab3 -n 100 -m\n");
+                printf("eg : ./aca_lab3 -h\n");
+                break;
+            case '?':
+                if (optopt == 'n') {
+                    fprintf(stderr, "Option -n requires an integer point argument\n");
+                } else {
+                    fprintf(stderr, "Unknown option character\n");
+                }
+                return 1;
+            default:
+                abort();
+        }
     }
-    printf("\nLoop unrolled simple -- OK\n");
+    // matrix creation
+    mat0 = matrixCreationNByN(n);
 
-    printf("\n\nLoop unrolled SSE calculation\n");
-//    test(n, vec_sse, mat, vec);
-    matvec_unrolled_sse(n, vec_sse, mat, vec);
-    printf("Loop unrolled SSE3  Answer\n");
-    for (int i = 0; i < n; ++i) {
-        printf("%f ", vec_sse[i]);
+    if (mat_vec_ver) {
+        printf("Program will create %d x %d matrix and a %dx1 vector for calculations\n", n, n, n);
+        // vector creation
+        in_vec = vectorCreation(n);
+        printf("Input Matrix\n");
+        printNByNMat(mat0, n);
+        printf("Input Vector\n");
+        printVector(in_vec, n);
+        // run 10 times get the average time
+        out_vec = (float *) malloc(sizeof(float)*n);
+        if(c_ver) {
+            printf("Running simple version\n");
+            driveMatVecCPU(mat0, in_vec, out_vec, n);
+            printf("Output Vector\n");
+            printVector(out_vec, n);
+        }
+        if(sse_ver){
+            printf("Running sse version\n");
+        }
+        if(a_vec_ver) {
+            printf("Running auto vectorized version\n");
+
+        }
+        free((float *)in_vec);
+        free((float *)out_vec);
+    } else if (mat_mat_ver) {
+        printf("Program will create two %d x %d matrices for calculations\n", n, n);
+        mat1 = matrixCreationNByN(n);
+        freeNByNMat((float **)mat1,n);
     }
 
-
-    printf("\nLoop unrolled SSE3  -- OK\n");
-    printf("Programme ran...\n");
+    freeNByNMat((float **)mat0, n);
     return 0;
 }
