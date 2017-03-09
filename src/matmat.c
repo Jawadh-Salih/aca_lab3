3c545b0695ec123d446b85e4bac13b6cb9a522e7
KRVPerera <rukshan.viduranga@gmail.com>
2017-03-09 10:32:56 +0530

    Added verification phase and simple bug fixes
    Signed-off-by: KRVPerera <rukshan.viduranga@gmail.com>

--- src/matmat.c
+++ src/matmat.c
@@ -1,4 +1,20 @@
 //
 // Created by jawadhsr on 2/27/17.
 //
-
+/**
+ * Listing 7 - Matrix multiplication
+ * @param n : dimension of the nxn matrix
+ * @param mat_c : output matrix
+ * @param mat_a : input matrix A
+ * @param mat_a : input matrix B
+ */
+void matmat_listing7(int n, float **mat_c,
+                     const float **mat_a, const float **mat_b) {
+    for (int i = 0; i < n; i++) {
+        for (int k = 0; k < n; k++) {
+            for (int j = 0; j < n; j++) {
+                mat_c[i][j] += mat_a[i][k] * mat_b[k][j];
+            }
+        }
+    }
+}
--- src/matvec_AUTO.c
+++ src/matvec_AUTO.c
@@ -2,3 +2,13 @@
 // Created by jawadhsr on 2/27/17.
 //
 
+void matvec_unrolled_auto(int n, float *vec_c, const float **mat_a, const float *vec_b) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j += 4) {
+            vec_c[i] += mat_a[i][j] * vec_b[j]
+                        + mat_a[i][j + 1] * vec_b[j + 1]
+                        + mat_a[i][j + 2] * vec_b[j + 2]
+                        + mat_a[i][j + 3] * vec_b[j + 3];
+        }
+    }
+}
